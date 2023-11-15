b4c3c17aa9ec2f824d47e0da5e89e7e1a17a7279
rihab44 <rihabkefi40@gmail.com>
2023-11-15 17:02:52 +0100

    first

---
+++ Student.java
@@ -0,0 +1,23 @@
+package studentsystem ;
+
+/*
+ * @author person1
+*/
+
+public class Student {
+    private String name;
+    private int studentId;
+
+    public Student(String name, int studentId) {
+        this.name = name;
+        this.studentId = studentId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+}
